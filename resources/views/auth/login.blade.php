ee1871dcc0c66ea16596bccffb7e5346eef1ebbe
Rahulkoju <blcraz75@gmail.com>
2024-10-24 18:58:10 +0545

    Login system from scratch

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,34 @@
+<x-layout>
+    <x-slot:heading>
+        Login
+    </x-slot:heading>
+    <form method="POST" action="/login">
+        @csrf
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <x-form-feild>
+                        <x-form-label for="email">Email</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input required name="email" id="email" placeholder="example@gmail.com" />
+                            <x-form-error name="email" />
+                        </div>
+                    </x-form-feild>
+
+                    <x-form-feild>
+                        <x-form-label for="password">Password</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input required type="password" name="password" id="password" />
+                            <x-form-error name="password" />
+                        </div>
+                    </x-form-feild>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+            <x-form-button>Login</x-form-button>
+        </div>
+    </form>
+</x-layout>
