0e1279d5fca0fc9c0208e823089308174ed41373
Rahulkoju <blcraz75@gmail.com>
2024-10-24 23:39:19 +0545

    Authorization mastery

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,6 @@ Route::view('/', 'home'); //3.route::view
 // });
 Route::view('/contact', 'contact');
 
-
-// Route::get("/jobs", [JobController::class, 'index']);
-// Route::get('/jobs/create', [JobController::class, 'create']);
-// Route::post('/jobs', [JobController::class, 'store']);
-// Route::get("/jobs/{job}", [JobController::class, 'show']);
-// Route::patch("/jobs/{job}", [JobController::class, 'update']);
-// Route::delete("/jobs/{job}", [JobController::class, 'delete']);
-// Route::get("/jobs/{job}/edit", [JobController::class, 'edit']);
-
 //4. route lists- php artisan route:list --except-vendor
 
 // Route::controller(JobController::class)->group(function () {  //5.route groups
@@ -36,12 +27,21 @@ Route::view('/contact', 'contact');
 //     Route::get("/jobs/{job}/edit", 'edit');
 // });
 
-Route::resource('jobs', JobController::class); //6.route resource
+// Route::resource('jobs', JobController::class); //6.route resource
 
+Route::get("/jobs", [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+Route::get("/jobs/{job}", [JobController::class, 'show']);
+Route::patch("/jobs/{job}", [JobController::class, 'update'])->middleware('auth');
+Route::delete("/jobs/{job}", [JobController::class, 'destroy'])->middleware('auth');
+Route::get("/jobs/{job}/edit", [JobController::class, 'edit'])
+    ->middleware('auth')
+    ->can('edit', 'job');
 
 Route::get("/register", [RegisteredUserController::class, 'create']);
 Route::post("/register", [RegisteredUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
