e9610c15f0abd9feee40fe2137afa0402f74c80c
Rahulkoju <blcraz75@gmail.com>
2024-10-25 15:50:20 +0545

    build process in order

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors:{
+                "brand":"rgb(500,138,241)"
+            }
         },
     },
     plugins: [],
